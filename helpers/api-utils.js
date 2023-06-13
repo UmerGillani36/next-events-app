4acb7dbf9c5dfa1888ba480cef5d829ae297463c
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 13:48:11 +0500

    firebase setup and get featured events

---
+++ helpers/api-utils.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://next-course-ef833-default-rtdb.firebaseio.com/events.json'
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
