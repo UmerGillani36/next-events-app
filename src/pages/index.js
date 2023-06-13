6bed9656ebf5adf102232422bf2f1519e4a4cbb0
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 13:47:55 +0500

    home with get static props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import EventList from '../../components/events/EventList';
-import { getAllEvents } from '../../dummyData';
-
-export default function Home() {
-  const allEvents = getAllEvents();
+import { getFeaturedEvents } from '../../helpers/api-utils';
+export default function Home(props) {
+  const { events } = props;
   return (
     <div>
-      <EventList items={allEvents} />
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
