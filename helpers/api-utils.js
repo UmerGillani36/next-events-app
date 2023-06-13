90f1c8d78c835aea9150e10f6bff5a5542ec6331
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 13:49:10 +0500

    get event by id

--- helpers/api-utils.js
+++ helpers/api-utils.js
@@ -20,3 +20,8 @@ export async function getFeaturedEvents() {
   const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
 }
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
