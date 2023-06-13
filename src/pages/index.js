9116a576593426a77670a1f65b8cf6c4af17cb1c
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 18:46:51 +0500

    head in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import EventList from '../../components/events/EventList';
 import { getFeaturedEvents } from '../../helpers/api-utils';
 export default function Home(props) {
   const { events } = props;
   return (
     <div>
+      <Head>
+        <title>Featured Events Page</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={events} />
     </div>
   );
