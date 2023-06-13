5b7487e39e52534e9040f8533700a976efc068f3
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 18:46:42 +0500

    head in root

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,21 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import Layout from '../../components/layout/layout';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Events App</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width= device-width"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
