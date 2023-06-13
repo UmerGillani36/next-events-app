a08468043d37b85615f61178df856496903bd0b9
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-13 18:47:23 +0500

    head in events

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -2,6 +2,7 @@ import EventList from '../../../components/events/EventList';
 import EventSearch from '../../../components/events/EventSearch';
 import { getAllEvents } from '../../../helpers/api-utils';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 const EventsPage = (props) => {
   const { featuredEvents } = props;
   const router = useRouter();
@@ -11,6 +12,13 @@ const EventsPage = (props) => {
   };
   return (
     <>
+      <Head>
+        <title>All Events Page</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventSearch onSearch={findEventHandler} />
       <EventList items={featuredEvents} />
     </>
