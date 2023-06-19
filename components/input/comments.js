807eb5da88bc56722a46f6f29513874fe25558a0
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-19 19:08:30 +0500

    getting all comments integration done

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
@@ -8,7 +8,17 @@ function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
-
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    if (showComments) {
+      fetch(`/api/comments/${eventId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('All Dummy comments', data);
+          setComments(data.comments);
+        });
+    }
+  }, [showComments]);
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
@@ -34,7 +44,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList comments={comments} />}
     </section>
   );
 }
