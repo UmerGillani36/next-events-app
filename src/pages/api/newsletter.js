94a9a712114e46799a66a2a8747600585e52669a
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 17:14:35 +0500

    inserting data in mongo db

--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,11 +1,18 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address' });
       return;
     }
-
+    const uri =
+      'mongodb+srv://umergillani36:aJ1ZSYke7FT7tdge@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority';
+    const client = await MongoClient.connect(uri);
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+    client.close();
     console.log('User Email', userEmail);
     res.status(201).json({ message: 'Successfully created' });
   }
