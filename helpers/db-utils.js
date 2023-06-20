ec13d7229eedb5bc5321b7bb69def29fee694017
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 18:35:06 +0500

    halper

---
+++ helpers/db-utils.js
@@ -0,0 +1,20 @@
+import ResultsTitle from '../components/results-title/results-title';
+import { MongoClient } from 'mongodb';
+
+export const connectDatabase = async () => {
+  const uri =
+    'mongodb+srv://umergillani36:aJ1ZSYke7FT7tdge@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority';
+  const client = await MongoClient.connect(uri);
+  return client;
+};
+export const insertDocument = async (client, collection, data) => {
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(data);
+  return result;
+};
+
+export const getALLComments = async (client, collection, sort) => {
+  const db = client.db();
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+  return documents;
+};
