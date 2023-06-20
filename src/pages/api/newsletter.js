c7e0718fe478bd057b0cd5abf14bf6ebea022818
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 17:59:47 +0500

    email error handling

--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,5 +1,14 @@
 import { MongoClient } from 'mongodb';
-
+const connectDatabase = async () => {
+  const uri =
+    'mongodb+srv://umergillani36:aJ1ZSYke7FT7tdge@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority';
+  const client = await MongoClient.connect(uri);
+  return client;
+};
+const insertDocument = async (client, data) => {
+  const db = client.db();
+  await db.collection('emails').insertOne(data);
+};
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -7,12 +16,21 @@ async function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address' });
       return;
     }
-    const uri =
-      'mongodb+srv://umergillani36:aJ1ZSYke7FT7tdge@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority';
-    const client = await MongoClient.connect(uri);
-    const db = client.db();
-    await db.collection('emails').insertOne({ email: userEmail });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (err) {
+      res.status(500).json({ message: 'Connecting to the database failed' });
+      return;
+    }
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (err) {
+      res.status(500).json({ message: 'Inserting data failed' });
+      return;
+    }
+
     console.log('User Email', userEmail);
     res.status(201).json({ message: 'Successfully created' });
   }
