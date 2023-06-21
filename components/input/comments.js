e4eb832bac079d7654d02c0ec096be348a7b6600
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 23:58:01 -0700

    notification using context api fetching and posting

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,22 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
+  const context = useContext(NotificationContext)
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
   useEffect(() => {
     if (showComments) {
+       context.showNotification({
+      title:'Fetching all Comments...',
+      message:'Fetching all comments for this event.',
+      status:'pending'
+    })
       fetch(`/api/comments/${eventId}`)
-        .then((res) => res.json())
+        .then((res) =>{
+          if(res.ok){
+            return res.json()
+          }
+          return res.json().then((data)=>{
+            throw new Error(data.message || 'Something went wrong')
+          })
+          })
         .then((data) => {
           console.log('All Dummy comments', data);
           setComments(data.comments);
-        });
+           context.showNotification({
+      title:'Success',
+      message:'Comment fetched successfully.',
+      status:'success'
+    })
+           
+        }).catch((err)=>{
+           context.showNotification({
+      title:'Error',
+      message: err.messsage || 'Something went wrong',
+      status:'error'
+    })
+        })
     }
   }, [showComments]);
   function toggleCommentsHandler() {
@@ -25,6 +51,11 @@ function Comments(props) {
 
   function addCommentHandler(commentData) {
     // send data to API
+    context.showNotification({
+      title:'Adding Comments...',
+      message:'Adding comments for this event.',
+      status:'pending'
+    })
     fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -32,10 +63,29 @@ function Comments(props) {
         'Content-Type': 'application/json',
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if(res.ok){
+          return res.json()
+        }
+
+        return res.json().then((data)=>{
+          throw new Error(data.message || 'Something went wrong')
+        })
+      })
       .then((data) => {
         console.log('Comments Data', data);
-      });
+        context.showNotification({
+      title:'Success',
+      message:'Comment added successfully.',
+      status:'success'
+    })
+      }).catch((err)=>{
+        context.showNotification({
+      title:'Error',
+      message: err.messsage || 'Something went wrong',
+      status:'error'
+    })
+      })
   }
 
   return (
