c0e8cf4959c0bdd59584d106b28d11003b657a6a
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 23:58:42 -0700

    adding notification comp

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+import Notification from '../ui/Notification';
 import MainHeader from './main-header';
+import NotificationContext from '../../store/notification-context';
 
 const Layout = (props) => {
+  const context = useContext(NotificationContext);
+  const activeNotification = context.notification;
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification title={activeNotification.title} message={activeNotification.message} status={activeNotification.status}/>
+      )}
+
     </>
   );
 };
