7f3ce8c954fda272d97e8349c310adc17cf1fb8d
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-20 23:58:18 -0700

    notification using context api

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,20 +1,49 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
   const [email, setEmail] = useState('');
+  const context = useContext(NotificationContext);
   function registrationHandler(event) {
     event.preventDefault();
+
+    context.showNotification({
+      title:'Signing up...',
+      message:'Registration for newsletter.',
+      status:'pending'
+    })
     fetch('/api/newsletter', {
       method: 'POST',
       body: JSON.stringify({ email: email }),
       headers: { 'Content-Type': 'application/json' },
     })
-      .then((res) => res.json())
+      .then((res) =>{
+        if(res.ok){
+          return res.json();
+        }
+
+       return res.json().then((data)=>{
+          throw new Error(data.message || 'Something went wrong')
+        })
+      }
+       )
       .then((data) => {
         console.log('Data', data);
         setEmail('');
-      });
+         context.showNotification({
+      title:'Success',
+      message:'Successfully register for newsletter.',
+      status:'success'
+    })
+      }).catch((err)=>{
+  context.showNotification({
+     title:'Error!',
+     message:err.meesage || 'Something went wrong!!!',
+     status:'error'
+   })
+      })
+      
   }
 
   return (
